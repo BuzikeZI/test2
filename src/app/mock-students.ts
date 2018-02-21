b6edad4567c899eb8773d3c6bb2dce141982f228
BuzikeZI <zhenglyu@bu.edu>
2018-02-20 19:47:30 -0500

    Add files via upload

---
+++ src/app/mock-students.ts
@@ -0,0 +1,7 @@
+import { student } from './student';
+
+export const STUDENTS: student[] = [
+	{id:1,name: 'Anna',gender: 'girl', math: 98, eng:95},
+	{id:2,name: 'Bob',gender: 'boy', math: 93, eng:90}
+
+];
